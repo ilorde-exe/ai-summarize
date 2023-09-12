d7a050e495fd582ad9481f1578f37099ba1b47a6
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2023-09-12 23:44:28 +0530

    Connect to Rapid Api

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const apiKey = import.meta.env.VITE_RAPIDAPI_KEY;
+
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", apiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => "test",
+      query: (params) =>
+        `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
