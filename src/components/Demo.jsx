ea9cafac56e9b5b1f8fe1fa5e53ca4a7821d76e3
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2023-09-12 22:57:33 +0530

    Setup states

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 
 const Demo = () => {
+  const [article, setArticle] = useState({ url: "", summary: "" });
+  const handleSubmit = () => {
+    
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src="src\assets\link.svg"
@@ -15,13 +19,20 @@ const Demo = () => {
           />
           <input
             type="url"
-            onChange={() => {}}
-            value=""
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            value={article.url}
             placeholder="Enter the url of the article..."
             required
             className="block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus:border-black focus:outline-none focus:ring-0 peer"
           />
-          <button type="submit"></button>
+          <button
+            type="submit"
+            className="hover:border-gray-700 hover:text-gray-700 absolute inset-y-0 right-0 my-1.5 mr-1.5 flex w-10 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400 peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↲
+          </button>
         </form>
       </div>
     </section>
